0c8db97915dd0895777389bd743fbc963f4d3ee8
Anthony Warnick <github@warnick.co>
2021-03-03 20:48:55 -0600

    all three loading

--- src/projectParagraph/index.js
+++ src/projectParagraph/index.js
@@ -1,12 +1,4 @@
-/**
- * Lets webpack process CSS, SASS or SCSS files referenced in JavaScript files.
- * All files containing `style` keyword are bundled together. The code used
- * gets applied both to the front of your site and to the editor.
- *
- * @see https://www.npmjs.com/package/@wordpress/scripts#using-css
- */
 import './style.scss';
-
 /**
  * Internal dependencies
  */
@@ -17,12 +9,9 @@ import save from './save';
 export const name = 'chu-gooding/project-paragraph';
 
 export const settings = {
-    title: __( 'C , G: Project Paragraph', 'project-paragraph' ),
-    description: __(
-		'Example block written with ESNext standard and JSX support – build step required.',
-		'project-paragraph'
-	),
-    category: 'widgets',
+    title: 'C , G: Project Paragraph',
+    description: 'Example block written with ESNext standard and JSX support – build step required.',
+    category: 'text',
     icon: 'smiley',
 	supports: {
 		// Removes support for an HTML mode.
